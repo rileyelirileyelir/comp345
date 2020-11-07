02520ade3c153c3aaf9add8e641e0a200f6c6661
rileyelirileyelir <69334895+rileyelirileyelir@users.noreply.github.com>
2020-11-07 03:24:11 -0500

    Update Player.h

--- A1/Player/Player.h
+++ A1/Player/Player.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <vector>
+#include <math.h>
 #include "Territory.h"
 #include "OrdersList.h"
 #include "Orders.h"
 #include "Map.h"
+#include "MapLoader.h"
 #include "Hand.h"
 
 
@@ -15,18 +17,25 @@ private:
     Hand _cards;
     OrdersList _playerOrderList;
 
-
-
 public:
+    
+    Player();
+    Player(std::vector<Territory>& territories, Hand cards);
+    std::vector<Territory> arbitraryTerritoriesToAttack; //TEMPORARY: For the first assignement
+    std::vector<Territory> toDefend();
+    std::vector<Territory> toAttack();
+    void issueOrder(int orderType);
+    void printPlayerOrders();
+    void printPlayerTerritories();
 
-   Player();
-   Player(std::vector<Territory> &territories ,Hand cards);
-   std::vector<Territory> arbitraryTerritoriesToAttack; //TEMPORARY: For the first assignement
-   std::vector<Territory> toDefend();
-   std::vector<Territory> toAttack();
-   void issueOrder(int orderType);
-   void printPlayerOrders();
-  void printPlayerTerritories();
+    //Added
+    int getTerritories();
+    void reinforcementPhase();
+    bool getAllTerritories(Continent& continent);
+    int distributeArmies(Territory& territory);
+    void issueOrdersPhase();
+    void beforeAttack(Territory& territory);
+    void beforeDefend(Territory& territory);
 
-
+    //Added
 };
